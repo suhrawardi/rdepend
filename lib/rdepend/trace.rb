b41425248285d6469ab16dff0cfbca05b80cc21f
jarra <suhrawardi@gmail.com>
2015-07-20 22:30:06 +0200

    Now we use set_trace_func.

--- lib/rdepend/trace.rb
+++ lib/rdepend/trace.rb
@@ -15,15 +15,22 @@ module Rdepend
 
     def self.start
       Rdepend::Event::Root.new(root_event)
-      @trace = TracePoint.new(:call) do |tp|
-        #Rdepend::Event.new(tp, caller_locations(2,1).first)
-        Rdepend::Event.new(tp, caller_locations(2,9))
-      end
-      @trace.enable
+      set_trace_func proc { |event, file, line, id, binding, classname|
+        if event == 'call' && file =~ /workspace/
+          printf("A)\t%8s %s:%-2d %10s %8s\n", event, file, line, id, classname)
+          tp = OpenStruct.new(event: event, path: file, lineno: line, method_id: id, defined_class: classname)
+          Rdepend::Event.new(tp, caller_locations(2,9))
+        end
+      }
+#      @trace = TracePoint.new(:call) do |tp|
+#        printf("B)\t%8s %s:%-2d %10s %8s\n", tp.event, tp.path, tp.lineno, tp.method_id, tp.defined_class)
+#        #Rdepend::Event.new(tp, caller_locations(2,9))
+#      end
+#      @trace.enable
     end
 
     def self.stop
-      @trace.disable
+#      @trace.disable
       Rdepend::Graph::Creator.instance.print
     end
 
--- rdepend.gemspec
+++ rdepend.gemspec
@@ -8,8 +8,8 @@ Gem::Specification.new do |spec|
   spec.version       = Rdepend::VERSION
   spec.authors       = ['jarra']
   spec.email         = ['suhrawardi@gmail.com']
-  spec.summary       = %q(TODO: Write a short summary. Required.)
-  spec.description   = %q(TODO: Write a longer description. Optional.)
+  spec.summary       = %q(a work in progress)
+  spec.description   = %q(a work in progress)
   spec.homepage      = ''
   spec.license       = 'MIT'
 
